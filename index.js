e65e803af494b6704fb8de2ec3a5a5cc8203b7b9
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-10-25 00:24:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('es6-shim'); //For the Math object (Math.logp1 and Math.expm1)
+require('es6-shim'); //For the Math object (Math.log1p and Math.expm1)
 
 module.exports = {
   dnorm: require('./lib/dnorm'),
